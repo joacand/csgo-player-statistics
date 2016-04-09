28696acee2ad08c3e94f1a0664e505f0902495cd
joacand <tbsitu@gmail.com>
2016-04-09 14:56:55 +0200

    Refactor tests, Add two new tests

--- test/test.js
+++ test/test.js
@@ -23,40 +23,50 @@ describe('processSteamIds function tests', function() {
     });
 
     it('should return an empty lists on empty input', function(done) {
-      cs.processSteamIds("", function(playerStats, banStats) {
-        assert.deepEqual([], playerStats);
-        assert.deepEqual([], banStats);
-        done();
-      });
+      testLengthOfOutput("", 0, done);
     });
 
     it('should return an empty lists on invalid input', function(done) {
-      cs.processSteamIds("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis elementum gravida dolor,\n" +
+      testLengthOfOutput("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis elementum gravida dolor,\n" +
         "a sollicitudin elit molestie eget. Morbi a aliquam est. Duis porta metus nulla, ut luctus nisi imperdiet nec.\n" +
         "Suspendisse euismod eget nibh quis pulvinar. Nunc ullamcorper quis nulla sit amet mollis.\n" +
-        "Nam accumsan ultrices commodo. Sed molestie justo.", function(playerStats, banStats) {
-        assert.deepEqual([], playerStats);
-        assert.deepEqual([], banStats);
-        done();
-      });
+        "Nam accumsan ultrices commodo. Sed molestie justo.", 0, done);
     });
 
     it('should return a list of length one for one valid player', function(done) {
-      cs.processSteamIds('# 1 1 "ExamplePlayer" STEAM_1:1:19846328 00:00 00 0 active 80000', function(playerStats, banStats) {
-        assert.deepEqual(1, playerStats.length);
-        assert.deepEqual(1, banStats.length);
-        done();
-      });
+      testLengthOfOutput('# 1 1 "ExamplePlayer" STEAM_1:1:19846328 00:00 00 0 active 80000', 1, done);
     });
 
     it('should return a list of length three for three valid players', function(done) {
-      cs.processSteamIds('# 1 1 "ExamplePlayer1" STEAM_1:1:19846328 00:00 00 0 active 80000\n'+
+      testLengthOfOutput('# 1 1 "ExamplePlayer1" STEAM_1:1:19846328 00:00 00 0 active 80000\n'+
         '# 1 1 "ExamplePlayer2" STEAM_1:1:19846329 00:00 00 0 active 80000\n'+
-        '# 1 1 "ExamplePlayer3" STEAM_1:1:19846330 00:00 00 0 active 80000', function(playerStats, banStats) {
-        assert.deepEqual(3, playerStats.length);
-        assert.deepEqual(3, banStats.length);
+        '# 1 1 "ExamplePlayer3" STEAM_1:1:19846330 00:00 00 0 active 80000', 3, done);
+    });
+
+    function testLengthOfOutput(input, length, done) {
+      cs.processSteamIds(input, function(playerStats, banStats) {
+        assert.deepEqual(length, playerStats.length);
+        assert.deepEqual(length, banStats.length);
         done();
       });
+    }
+
+    it('should be able to parse players with quotation marks in their nick', function(done) {
+      var nick = 'Example "12" Player'
+      testNick(nick, done);
     });
+
+    it('should be able to parse players with single quotation marks in their nick', function(done) {
+      var nick = "Example '12' Player"
+      testNick(nick, done);
+    });
+
+    function testNick(nickToTest, done) {
+      cs.processSteamIds("# 1 1 \""+nickToTest+"\" STEAM_1:1:19846330 00:00 00 0 active 80000", function(playerStats, banStats) {
+        assert.deepEqual(nickToTest, playerStats[0].nick);
+        assert.deepEqual(nickToTest, banStats[0].nick);
+        done();
+      });
+    }
   })
 });
